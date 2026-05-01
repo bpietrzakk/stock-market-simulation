485210b15b1649ba741e98556ec0f7f5ed5e060e
bpietrzakk <bpietrzak109@gmail.com>
2026-05-01 15:00:28 +0200

    feat: add GET /log endpoint - returns entire audit log

---
+++ src/main/java/com/bpietrzak/stockmarket/controller/AuditLogController.java
@@ -0,0 +1,28 @@
+package com.bpietrzak.stockmarket.controller;
+
+import com.bpietrzak.stockmarket.dto.AuditLogResponse;
+import com.bpietrzak.stockmarket.service.AuditLogService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+public class AuditLogController {
+    private final AuditLogService auditLogService;
+
+    // CONSTRUCTOR
+    public AuditLogController(AuditLogService auditLogService) {
+        this.auditLogService = auditLogService;
+    }
+
+    // ENDPOINTS
+    // GET: returns entire audit log in order of occurrence
+    @GetMapping
+    public ResponseEntity<AuditLogResponse> getLog() {
+        AuditLogResponse log = auditLogService.getLog();
+
+        return ResponseEntity.ok(log);
+    }
+}
