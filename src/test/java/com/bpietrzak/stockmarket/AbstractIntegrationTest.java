9f9c321b050980df7172441881431a328217829a
bpietrzakk <bpietrzak109@gmail.com>
2026-05-03 17:10:20 +0200

    AbstractIntegrationTest fixed

--- src/test/java/com/bpietrzak/stockmarket/AbstractIntegrationTest.java
+++ src/test/java/com/bpietrzak/stockmarket/AbstractIntegrationTest.java
@@ -3,14 +3,15 @@ package com.bpietrzak.stockmarket;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest // run all spring boot context
-@Testcontainers // look for containers and manage their life in this class
+@SpringBootTest
 public abstract class AbstractIntegrationTest {
 
-    @Container // docker container manage
-    @ServiceConnection // automatically configure SpringDataURL to connect
+    @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    // start once for all test classes, stop on JVM shutdown
+    static {
+        postgres.start();
+    }
 }
